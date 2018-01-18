15809a82da6774c9ae5168c3f264dedf0fbc3263
mdelacruz <marvin.delacruz@seer-technologies.com>
2018-01-18 13:51:10 +0800

    imports

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import createStore from './store/createStore'
 import './styles/main.scss'
+require('bootstrap/dist/js/bootstrap.bundle.js')
+var $ = require('jquery');
+window.$ = $;
+
 
 // Store Initialization
 // ------------------------------------
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,11 +2,31 @@ import React from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
-  </div>
-)
+
+class HomeView extends React.Component {
+	render() {
+		return (
+			<div>
+				<ul className="nav nav-tabs" id="myTab" role="tablist">
+				  <li className="nav-item">
+				    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+				  </li>
+				  <li className="nav-item">
+				    <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+				  </li>
+				  <li className="nav-item">
+				    <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+				  </li>
+				</ul>
+				<div className="tab-content" id="myTabContent">
+				  <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+				  <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+				  <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+				</div>
+			</div>
+		)
+	}
+
+}
 
 export default HomeView
